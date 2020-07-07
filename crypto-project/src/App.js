b0330234348bc23ba7c7614ce68005cfad3c79d6
antguts <antgut035@gmail.com>
2020-07-07 17:42:45 -0400

    Connected axios to sandbox environment for testing

--- crypto-project/src/App.js
+++ crypto-project/src/App.js
@@ -11,35 +11,13 @@ const App =()=> {
 
   axios
     .get(
-      "https://cors-anywhere.herokuapp.com/https://pro-api.coinmarketcap.com/v1/cryptocurrency/map?CMC_PRO_API_KEY=a54b0c17-c6cd-43c8-8610-c34563bd6028"
+      "https://cors-anywhere.herokuapp.com/https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=16e97361-585c-4270-bb8b-1f2a39fc956a"
     )
     .then((res) => console.log(res))
     .catch((err) => console.log(err));
+  // =================================================================
 
 
-  // const rp = require('request-promise');
-  // const requestOptions = {
-  //   method: 'GET',
-  //   uri: 'https://cors-anywhere.herokuapp.com/https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest',
-  //   qs: {
-  //     'start': '1',
-  //     'limit': '5000',
-  //     'convert': 'USD'
-  //   },
-  //   headers: {
-  //     'X-CMC_PRO_API_KEY': 'a54b0c17-c6cd-43c8-8610-c34563bd6028'
-  //   },
-  //   json: true,
-  //   gzip: true
-  // };
-  
-  // rp(requestOptions).then(response => {
-  //   console.log('API call response:', response);
-  // }).catch((err) => {
-  //   console.log('API call error:', err.message);
-  // });
-
-  // ======================================================================
   
   
   
--- crypto-project/src/components/home/Home.js
+++ crypto-project/src/components/home/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
     return (
         <div>
-            <img src='https://dspfdd2n9fb8f.cloudfront.net/wp-content/uploads/2019/12/Bitcoin-Stock-to-Flow-Model-The-Cryptocurrency-Market-Tool.jpg' />
+            <img src='https://dspfdd2n9fb8f.cloudfront.net/wp-content/uploads/2019/12/Bitcoin-Stock-to-Flow-Model-The-Cryptocurrency-Market-Tool.jpg' alt="[coinPlacholder]"/>
            <h1>All of our Graphs and stuff can go here</h1>
         </div>
     )
