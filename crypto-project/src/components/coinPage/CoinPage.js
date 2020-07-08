f9c44ec1757d01d9da68d7dba708ac582d1863fd
Shane Schipper <shane99@vt.edu>
2020-07-08 15:52:12 -0400

    Added graphs

--- crypto-project/src/components/coinPage/CoinPage.js
+++ crypto-project/src/components/coinPage/CoinPage.js
@@ -1,23 +1,23 @@
 import React, { Component } from 'react';
 import Navbar from './../nav/Navbar'
-
+import 'bulma/css/bulma.css'
 
 const CoinPage =(props)=> {
 
+    //style={{marginBottom: '15px', backgroundColor: 'lightgray'}}
 
     const displayInfo =()=>{
         // let c=props.coins[0];
         // let lastUpdate=new Date(c.last_updated)
         return props.coins.map((eachCoin)=>{
             return (
-                <div style={{marginBottom: '15px'}} key={eachCoin.id}>
-                    <h1>{eachCoin.name}</h1>
+                <div key={eachCoin.id} className='box isDark'>
+                    <h1>{eachCoin.name} / {eachCoin.symbol}</h1>
                     <h3>Price: <span>${eachCoin.quote.USD.price.toFixed(2)}</span></h3>
                     <h3>Last Updated: <span>{eachCoin.last_updated}</span></h3>
                     <h3>Circulating Supply: <span>{eachCoin.circulating_supply}</span></h3>
                     <h3>Total Supply: <span>{eachCoin.total_supply}</span></h3>
                     <h3>Max Supply: <span>{eachCoin.max_supply}</span></h3>
-                    <h1>{eachCoin.symbol}</h1>
                 </div>                
             )
         })
--- crypto-project/src/components/home/Home.js
+++ crypto-project/src/components/home/Home.js
@@ -2,14 +2,18 @@ import React from 'react'
 import homeImg from './homeImg.png'
 import { Link } from 'react-router-dom';
 import Navbar from './../nav/Navbar.js'
-
+import { LineChart, Line } from 'recharts';
+import Graph from '../Graph'
 
 const Home = () => {
-    const heroImg={
+    const heroImg = {
         backgroundImage: `url(${homeImg})`,
         width: '100%',
         height: '530px'
     }
+
+    const data = [{ name: 'Page A', uv: 400 }, { name: 'Page B', uv: 400 }, { name: 'Page C', uv: 400 }, { name: 'Page D', uv: 425 }];
+
 
     return (
         <div>
@@ -17,6 +21,11 @@ const Home = () => {
             <div style={heroImg}>
             </div>
             <Link to="/coin">Go to coin</Link>
+            {/* <LineChart width={400} height={400} data={data}>
+                <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+            </LineChart> */}
+            <Graph />
+            <h1>Hello</h1>
         </div>
     )
 }
