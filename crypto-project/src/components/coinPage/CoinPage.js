e2558c93d34a1ec0b468925e56973214459be40e
antguts <antgut035@gmail.com>
2020-07-08 12:46:06 -0400

    update

--- crypto-project/src/components/coinPage/CoinPage.js
+++ crypto-project/src/components/coinPage/CoinPage.js
@@ -8,36 +8,35 @@ const CoinPage =(props)=> {
     const displayInfo =()=>{
         // let c=props.coins[0];
         // let lastUpdate=new Date(c.last_updated)
-        return props.coins.map((eachBeer)=>{
+        return props.coins.map((eachCoin)=>{
             return (
-                <div style={{marginBottom: '15px'}} kay={eachBeer.id}>
-                    <h1>{eachBeer.name}</h1>
-                    <h3>Price: <span>${eachBeer.quote.USD.price.toFixed(2)}</span></h3>
-                    <h3>Last Updated: <span>{eachBeer.last_updated}</span></h3>
-                    <h3>Circulating Supply: <span>{eachBeer.circulating_supply}</span></h3>
-                    <h3>Total Supply: <span>{eachBeer.total_supply}</span></h3>
-                    <h3>Max Supply: <span>{eachBeer.max_supply}</span></h3>
-                    <h1>{eachBeer.symbol}</h1>
+                <div style={{marginBottom: '15px'}} key={eachCoin.id}>
+                    <h1>{eachCoin.name}</h1>
+                    <h3>Price: <span>${eachCoin.quote.USD.price.toFixed(2)}</span></h3>
+                    <h3>Last Updated: <span>{eachCoin.last_updated}</span></h3>
+                    <h3>Circulating Supply: <span>{eachCoin.circulating_supply}</span></h3>
+                    <h3>Total Supply: <span>{eachCoin.total_supply}</span></h3>
+                    <h3>Max Supply: <span>{eachCoin.max_supply}</span></h3>
+                    <h1>{eachCoin.symbol}</h1>
                 </div>                
             )
         })
     }//end display
 
+    // const coinBanner =()=>{
+    //     return props.coins.map((each)=>{
+    //         each.symbol+' '+each.quote.USD.price.toFixed(2)+'  '
+    //     })
+        
+    // }
 
-    const coinBanner =()=>{
-        return props.coins.map((each, total)=>{
-            total+= each.symbol +' '+each.quote.USD.price.toFixed(2)+'  '
-        })
-    }
 
-
-    console.log(props.coins[0])
     return (
         <div>
             <Navbar />               
             <br />
             {displayInfo()}
-            {coinBanner()}
+            {/* {coinBanner()} */}
         </div>
     );
 }
