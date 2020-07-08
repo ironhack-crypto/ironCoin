ba812a2eaed71c0b9eff9e5976a1bc21c6585b92
antguts <antgut035@gmail.com>
2020-07-08 12:09:00 -0400

    updated routes and added props

--- crypto-project/src/App.js
+++ crypto-project/src/App.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react'
+import React, { Component } from 'react'
 import { Link, Switch, Route } from 'react-router-dom';
 import './App.css'
 import 'bulma/css/bulma.css'
@@ -7,33 +7,42 @@ import Home from './components/home/Home'
 import CoinPage from './components/coinPage/CoinPage.js'
 import axios from 'axios'
 
-const App =()=> {
+class App extends Component{
   
-  //SERVER PROMISE-REQUEST==========================================
+  state={
+    coins: []
+  }
 
-  axios
-    .get(
-      "https://cors-anywhere.herokuapp.com/https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=16e97361-585c-4270-bb8b-1f2a39fc956a"
-    )
-    .then((res) => console.log(res))
-    .catch((err) => console.log(err));
+  //SERVER PROMISE-REQUEST==========================================
+  componentDidMount(){
+    axios
+      .get(
+        "https://cors-anywhere.herokuapp.com/https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=16e97361-585c-4270-bb8b-1f2a39fc956a"
+      ).then(response=>{
+          this.setState({ //set state to coin list. Data twice because of how the objects are designed in API
+          coins: response.data.data
+        })//end setstate
+      })//end then
+      .catch((err) => console.log(err));
+  }
   // =================================================================
 
 
   
   
   
-  
+  render(){
+    
     return (
       <div >
         {/* No Renders in App.js. Routes only */}
         <Switch>
           <Route exact path="/" render={(props)=><Home {...props}/>} />
-          <Route path="/coin" render={CoinPage} />
+          <Route path="/coin" render={(props)=><CoinPage coins={this.state.coins} {...props}/>}/>
         </Switch>
       </div>
     );
-  
+  }
 }
 
 export default App;
--- crypto-project/src/components/coinPage/CoinPage.js
+++ crypto-project/src/components/coinPage/CoinPage.js
@@ -2,26 +2,38 @@ import React, { Component } from 'react';
 import Navbar from './../nav/Navbar'
 
 
-const CoinPage =()=> {
+const CoinPage =(props)=> {
 
 
     const displayInfo =()=>{
+        let c=props.coins[0];
+        // let lastUpdate=new Date(c.last_updated)
         return <div>
-            <h1>[Name]</h1>
-            <h3>Price: <span>00</span></h3>
-            <h3>Last Updated: <span>00</span></h3>
-            <h3>Circulating Supply: <span>00</span></h3>
-            <h3>Total Supply: <span>00</span></h3>
-            <h3>Max Supply: <span>00</span></h3>
-            <h1>[Symbol]</h1>
+            <h1>{c.name}</h1>
+            <h3>Price: <span>${c.quote.USD.price.toFixed(2)}</span></h3>
+            <h3>Last Updated: <span>{c.last_updated}</span></h3>
+            <h3>Circulating Supply: <span>{c.circulating_supply}</span></h3>
+            <h3>Total Supply: <span>{c.total_supply}</span></h3>
+            <h3>Max Supply: <span>{c.max_supply}</span></h3>
+            <h1>{c.symbol}</h1>
         </div>
     }
 
+
+    const coinBanner =()=>{
+        return props.coins.map((each, total)=>{
+            total+= each.symbol +' '+each.quote.USD.price.toFixed(2)+'  '
+        })
+    }
+
+
+    console.log(props.coins[0])
     return (
         <div>
             <Navbar />               
             <br />
             {displayInfo()}
+            {coinBanner()}
         </div>
     );
 }
