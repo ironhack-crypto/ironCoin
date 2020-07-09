ad674ea807a9137a827b90bf2b74d45f39bf6ceb
killshot13 <dmreh@outlook.com>
2020-07-09 15:18:43 -0500

    added fixed navigation bar with buttons (links not wired up yet)

--- crypto-project/src/components/nav/Navbar.js
+++ crypto-project/src/components/nav/Navbar.js
@@ -1,13 +1,28 @@
 import React from 'react'
 import { Link, Switch, Route } from 'react-router-dom';
-import { Nav } from 'reactbulma';
+import 'bulma/css/bulma.css';
+import { Button, Control, Field, Icon, Nav, NavCenter, NavItem, NavLeft, NavRight, NavToggle } from 'reactbulma';
 
 const Navbar = () => {
     return (
-        <nav>
-            <Link to="/" style={{ Nav }} />
-        </nav>
-    )
+    <div style={{background: '#282a36', position: 'fixed', padding: '10px', width: '100%' }}>
+        <Nav>
+            <Nav.Center>
+                <Nav.Item>
+                    <Field groupedCentered>
+                        <Control>
+                            <Button dark inverted outlined>View Coins</Button>
+                        </Control>
+                        <Control>
+                            <Button dark inverted outlined>View Tickers</Button>
+                        </Control>
+                    </Field>
+                </Nav.Item>
+            </Nav.Center>
+        </Nav>
+    </div>
+    );
 }
-export default Navbar 
-
+                 
+export default Navbar
+    
