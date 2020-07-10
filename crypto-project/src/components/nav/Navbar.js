c948a7ac1074cc7b4251ba72f6ec36fc81f09e5b
Anthony Gutilla <antgut035@gmail.com>
2020-07-10 14:24:05 -0400

    Update Navbar.js

--- crypto-project/src/components/nav/Navbar.js
+++ crypto-project/src/components/nav/Navbar.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
 import 'bulma/css/bulma.css';
 import { Link } from 'react-router-dom';
 import { Button, Level, Nav, NavCenter, NavItem } from 'reactbulma';
